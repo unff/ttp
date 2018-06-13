974bbeff6e26365c80b4f9efe1e79ea601287211
JC Ryan <jc_ryan@live.com>
2018-06-13 00:45:18 -0400

    comments

--- timetrade.php
+++ timetrade.php
@@ -1,13 +1,15 @@
 <?php
+// pull password out of $_GET (base64 encoded?)
 $configFile = "./tt.json";
+$id = 0;
     // Make sure settings file exists
     touch('./tt.json');
     if(!filesize($configFile)) {
         echo 'empty';
-        // file is empty, write defaults to file
+        // file is empty, write defaults to config file
         $config = (object)array(
-            'c1' => '',
-            'c2' => '',
+            'cookie1' => '',
+            'cookie2' => '',
             'exp' => time()
         );
         $cFile = fopen($configFile, "w");
@@ -22,18 +24,25 @@ $configFile = "./tt.json";
     // Check exp for freshness
     var_dump($json);
     if ($json->exp < time()) {
+        //expiration has passed. generate new credentials.
         echo 'exp < now';
-    } else {
-        echo 'exp > now';
-    }
-    // Get new salt
-    // Get new token with salt
+        //call for new salt
+        //call for new token
+        //calculate exp
+        //save token and exp to config file
+        // are curl calls sync or async?
+    } 
+
     // Get appt list
     // Return JSON object
     $id = 0;
     $pw = "Idiot3@2";
     $salt = "@n6`pfvicgyre2Oc";
-    $hashA = base64_encode(MD5($pw, true));
-    $hashB = base64_encode(MD5($hashA.$salt, true));
-    echo $hashB;
+    
+    function makeHash($pw, $salt) {
+        $hash = base64_encode(MD5($pw, true));
+        return base64_encode(MD5($hash.$salt, true));
+    }
+    echo "\r\n";
+    echo makeHash($pw, $salt);
 ?>
